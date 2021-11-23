cd27fc99dd31521b9766130218ca94e298993f67
RunLJT <68778507+Runlertjit@users.noreply.github.com>
2021-11-24 00:21:35 +0700

    Add files via upload

---
+++ Group_work/101_Run/JS/search.js
@@ -0,0 +1,43 @@
+export { searching, showSearch };
+
+//Searching product.
+const search = document.querySelector('#buttonSch');
+search.addEventListener('click', searching);
+function searching() {
+  const productAll = document.getElementsByClassName("sProduct");
+  const searchString = document.querySelector("#SearchProduct");
+  const filter = searchString.value.toLowerCase();
+  for (let product of productAll) {
+    let name = product.textContent.toLocaleLowerCase();
+    if (name.includes(filter)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  }
+}
+
+
+
+//Fliped Search Bar.
+const searchBar = document.querySelector('#Simg');
+searchBar.addEventListener('click', showSearch);
+function showSearch() {
+  if (divWrap.style.display != "none") {
+    divWrap.style.display = "none";
+  } else {
+    divWrap.style.display = "inline";
+  }
+}
+
+
+//----------- Code เก่า ------------------ 
+//let count = 0;
+// function showSearch() {
+// count++
+//   if (count % 2 == 0) {
+//     divWrap.setAttribute('style', 'display: none;');
+//   } else {
+//     divWrap.setAttribute('style', 'display: inline;');
+//   }
+// }
